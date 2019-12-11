fadc5d39d5d7168e279ade8bb71699faea6c5774
Cindy Kei <myk245@nyu.edu>
2019-12-11 02:55:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,9 +7,9 @@ def run_guessing_game
   if user_input == "exit"
     puts "Goodbye!"
   elsif user_input == random_number
-      puts "You guessed the correct number!"
+    puts "You guessed the correct number!"
   else
-      puts "Sorry! The computer guessed #{random_number}."
+    puts "Sorry! The computer guessed #{random_number}."
 
-    end
+  end
 end
